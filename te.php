00fafc155db9f99fe414edb98f951633681148ef
jojuaswand <58320550+jojuaswand@users.noreply.github.com>
2021-02-04 20:13:21 +0530

    Add files via upload

---
+++ te.php
@@ -0,0 +1,25 @@
+<html>
+<body>
+<!-- <iframe class="videoplayer" src="http://www.twitch.tv/embed?channel=its_joju" height="478" width="800" frameborder="0" scrolling="no"></iframe>
+<iframe class="videoplayer" src="https://www.twitch.tv/videos/899536815" height="478" width="800" frameborder="0" scrolling="no"></iframe>
+<iframe
+    src="https://player.twitch.tv/videos/899536815"
+    height="400"
+    width="300"
+    allowfullscreen="<allowfullscreen>">
+</iframe>
+<iframe src="https://clips.twitch.tv/embed?clip=HandsomeObliqueMooseBloodTrail&parent=www.example.com" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
+https://clips.twitch.tv/HandsomeObliqueMooseBloodTrail -->
+<script src="twembedscript1.js"></script>
+
+    <!-- Create a Twitch.Embed object that will render within the "twitch-embed" root element. -->
+    <script type="text/javascript">
+      new Twitch.Embed("twitch-embed", {
+        width: 800,
+        height: 400,
+        channel: "its_joju",
+
+      });
+    </script>
+</body>
+</html>
